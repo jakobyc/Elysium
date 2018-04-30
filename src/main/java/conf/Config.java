9de7dc731c001d27e76542a3d59476377d167c01
Chad <chad3281@gmail.com>
2018-04-29 20:52:06 -0400

    Remove stackdump

--- src/main/java/conf/Config.java
+++ src/main/java/conf/Config.java
@@ -6,41 +6,41 @@ import main.java.serialization.Serializer;
 
 public class Config
 {
-        private static String path = System.getProperty("user.dir");
+    private static String path = System.getProperty("user.dir");
 
-        private static File configTemplate;
-        private static File config;
+    private static File configTemplate;
+    private static File config;
 
-        private static Settings settings;
+    private static Settings settings;
 
-        static
+    static
+    {
+        configTemplate = new File(path + "/src/main/java/conf/configTemplate.xml");
+        config = new File(path + "/src/main/java/conf/config.xml");
+
+        if (config.exists())
         {
-                configTemplate = new File(path + "/src/main/java/conf/configTemplate.xml");
-                config = new File(path + "/src/main/java/conf/config.xml");
-
-                if (config.exists())
-                {
-                        settings = Serializer.deserialize(Settings.class, config);
-                }
-                else
-                {
-                        settings = new Settings();
-                }
+            settings = Serializer.deserialize(Settings.class, config);
         }
-
-        public static Settings getSettings()
+        else
         {
-                if (config.exists() && config.isFile())
-                {
-                        return Serializer.deserialize(Settings.class, config);
-                }
-                return new Settings();
+            settings = new Settings();
         }
+    }
 
-        public static void setRoot(String root)
+    public static Settings getSettings()
+    {
+        if (config.exists() && config.isFile())
         {
-                settings.Root = root;
-
-                Serializer.serialize(settings, config);
+            return Serializer.deserialize(Settings.class, config);
         }
+        return new Settings();
+    }
+
+    public static void setRoot(String root)
+    {
+        settings.Root = root;
+
+        Serializer.serialize(settings, config);
+    }
 }
